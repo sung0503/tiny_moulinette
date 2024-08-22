d590c624f37ce42f7d178c86c1241737888092d6
Changhwan Sung <chsung@c2r2s2.42gyeongsan.kr>
2024-08-23 00:55:24 +0900

    C07

--- moulinette/tests/C07/ex04.c
+++ moulinette/tests/C07/ex04.c
@@ -1,6 +1,4 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 char *ft_convert_base(char *nbr, char *base_from, char *base_to);
 
--- moulinette/tests/C07/ex05.c
+++ moulinette/tests/C07/ex05.c
@@ -1,6 +1,4 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 char **ft_split(char *str, char *charset);
 
